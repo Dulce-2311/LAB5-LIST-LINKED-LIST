7f1a579311d7768abde4227bfe625c2ca59101d3
MauriceuMarquez <mauriceu.marquez@estudiante.ucsm.edu.pe>
2026-04-26 14:52:51 -0500

    Creación oficial del ejercicio E_02_InvertirLista

---
+++ List_Linked_List/src/Ejercicioss/E_02_invertirLista/InvertirLista.java
@@ -0,0 +1,21 @@
+package Ejercicioss.E_02_invertirLista;
+
+import estructuras.ListLinked;
+import estructuras.Node;
+
+public class InvertirLista {
+
+    public static <T> ListLinked<T> invertirLista(ListLinked<T> lista) {
+
+        ListLinked<T> nueva = new ListLinked<>();
+
+        Node<T> actual = lista.getFirst();
+
+        while (actual != null) {
+            nueva.insertFirst(actual.value);
+            actual = actual.next;
+        }
+
+        return nueva;
+    }
+}
---
+++ List_Linked_List/src/Ejercicioss/E_02_invertirLista/MainInvertir.java
@@ -0,0 +1,23 @@
+package Ejercicioss.E_02_invertirLista;
+
+import estructuras.ListLinked;
+
+public class MainInvertir {
+
+    public static void main(String[] args) {
+
+        ListLinked<Integer> lista = new ListLinked<>();
+
+        lista.insertLast(5);
+        lista.insertLast(10);   
+        lista.insertLast(20);
+
+        System.out.println("Lista original:");
+        lista.print();
+
+        ListLinked<Integer> invertida = InvertirLista.invertirLista(lista);
+
+        System.out.println("Lista invertida:");
+        invertida.print();
+    }
+}
